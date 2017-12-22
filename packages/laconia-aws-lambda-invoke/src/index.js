860a5693f9135d95e1778cffe793b227f533a1e9
Wisen Tanasa <wisen@ceilfors.com>
2017-12-22 09:46:08 +0000

    Initial implementation of invoker.

---
+++ packages/laconia-aws-lambda-invoke/src/index.js
@@ -0,0 +1,23 @@
+module.exports = class LambdaInvoker {
+  constructor(lambda, functionName) {
+    this.lambda = lambda;
+    this.functionName = functionName;
+  }
+
+  fireAndForget() {
+    return this.lambda
+      .invoke({ FunctionName: this.functionName })
+      .promise()
+      .then(data => {
+        if (data.FunctionError) {
+          throw new Error(
+            `${data.FunctionError} error returned by ${this.functionName}: ${
+              data.Payload
+            }`
+          );
+        } else {
+          return data;
+        }
+      });
+  }
+};

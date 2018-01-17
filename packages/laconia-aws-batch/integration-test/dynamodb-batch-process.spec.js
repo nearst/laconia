64b7574079cbd4d225af5988af48ab5d7fa72334
Wisen Tanasa <wisen@ceilfors.com>
2018-01-17 06:46:06 +0000

    Add new failing test.

--- packages/laconia-aws-batch/integration-test/dynamodb-batch-process.spec.js
+++ packages/laconia-aws-batch/integration-test/dynamodb-batch-process.spec.js
@@ -2,6 +2,7 @@
 
 const DynamoDbLocal = require('dynamodb-local')
 
+const AWSMock = require('aws-sdk-mock')
 const AWS = require('aws-sdk')
 const DynamoDbMusicRepository = require('./DynamoDbMusicRepository')
 const {BatchProcessor, DynamoDbItemReader} = require('../src/index')
@@ -9,6 +10,7 @@ const {BatchProcessor, DynamoDbItemReader} = require('../src/index')
 describe('aws invoke', () => {
   const dynamoLocalPort = 8000
   const dynamoDbOptions = { region: 'eu-west-1', endpoint: new AWS.Endpoint(`http://localhost:${dynamoLocalPort}`) }
+  let invokeMock
 
   beforeAll(() => {
     jest.setTimeout(5000)
@@ -25,6 +27,15 @@ describe('aws invoke', () => {
     await musicRepository.save({Artist: 'Foo'})
     await musicRepository.save({Artist: 'Bar'})
     await musicRepository.save({Artist: 'Fiz'})
+  })
+
+  beforeEach(() => {
+    invokeMock = jest.fn()
+    AWSMock.mock('Lambda', 'invoke', invokeMock)
+  })
+
+  afterEach(() => {
+    AWSMock.restore()
   })
 
   it('should process all records in a Table with scan', async () => {
@@ -55,7 +66,30 @@ describe('aws invoke', () => {
     expect(itemProcessor).toHaveBeenCalledTimes(1)
   })
 
-  it('should recurse when time is up')
+  xit('should recurse when time is up', async () => {
+    const functionName = 'foo'
+    const itemProcessor = jest.fn()
+    const batchProcessor = new BatchProcessor(
+      { functionName, getRemainingTimeInMillis: () => 3000 },
+      new DynamoDbItemReader(new AWS.DynamoDB.DocumentClient(dynamoDbOptions), {TableName: 'Music'}),
+      itemProcessor,
+      { timeNeededToRecurseInMillis: 5000 }
+    )
+    await batchProcessor.start()
+    expect(itemProcessor).toHaveBeenCalledTimes(1)
+    expect(invokeMock).toBeCalledWith(
+      expect.objectContaining({
+        FunctionName: functionName,
+        InvocationType: 'Event',
+        Payload: JSON.stringify({lastEvaluatedKey: {Artist: 'Fiz'}})
+      }),
+      expect.any(Function)
+    )
+  })
+
+  it('should forward event during recursion')
+
+  it('CachedDynamoDbItemReader should not use Limit: 1 or should we return an array and process it all? Then setting Limit is becoming the user responsibility')
 
   it('should not return unefined item when lastEvaluatedKey is not empty')
 

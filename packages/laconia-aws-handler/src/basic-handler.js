042632b6e2d8a0bc06ae71a03b2f9d8c752251b0
Wisen Tanasa <wisen@ceilfors.com>
2018-01-21 08:43:59 +0000

    Rename laconia handler to basic handler.

---
+++ packages/laconia-aws-handler/src/basic-handler.js
@@ -0,0 +1,6 @@
+module.exports = handler => (event, context, callback) => {
+  return Promise.resolve()
+    .then(_ => handler(event, context, callback))
+    .then(result => callback(null, result))
+    .catch(err => callback(err));
+};

65f379744abd02cdf077c55fe6a9779a42d2e261
Wisen Tanasa <wisen@ceilfors.com>
2018-02-07 06:30:39 +0000

    Clean code.

--- packages/laconia-aws-batch/src/DynamoDbItemReader.js
+++ packages/laconia-aws-batch/src/DynamoDbItemReader.js
@@ -23,28 +23,38 @@ module.exports = class DynamoDbItemReader {
       : this.documentClient.scan(params).promise();
   }
 
+  async _getDataFromDynamoDb(cursor) {
+    const data = await this._hitDynamoDb(this._createDynamoDbParams(cursor));
+    this.cachedItems = data.Items;
+    return data;
+  }
+
+  _getDataFromCache(cursor) {
+    return {
+      Items: this.cachedItems,
+      LastEvaluatedKey: cursor.lastEvaluatedKey
+    };
+  }
+
+  _hasNextItem(items, currentIndex) {
+    return currentIndex >= 0 && currentIndex + 1 <= items.length - 1;
+  }
+
+  _hasNextItemInCache(cursor) {
+    return this._hasNextItem(this.cachedItems, cursor.index);
+  }
+
   async next(cursor = { lastEvaluatedKey: undefined, index: -1 }) {
-    let data, index;
-
-    if (cursor.index < 0 || cursor.index + 1 > this.cachedItems.length - 1) {
-      data = await this._hitDynamoDb(this._createDynamoDbParams(cursor));
-      this.cachedItems = data.Items;
-      index = 0;
-    } else {
-      data = {
-        Items: this.cachedItems,
-        LastEvaluatedKey: cursor.lastEvaluatedKey
-      };
-      index = cursor.index + 1;
-    }
-
+    const [data, index] = this._hasNextItemInCache(cursor)
+      ? [this._getDataFromCache(cursor), cursor.index + 1]
+      : [await this._getDataFromDynamoDb(cursor), 0];
     const { Items: items, LastEvaluatedKey: lastEvaluatedKey } = data;
-    const item = items[index];
 
     return {
-      item,
+      item: items[index],
       cursor: { lastEvaluatedKey, index },
-      finished: lastEvaluatedKey === undefined && index + 1 > items.length - 1
+      finished:
+        lastEvaluatedKey === undefined && !this._hasNextItem(items, index)
     };
   }
 };

e9e815ef3bea3bf875617ae411920fa95fc4758d
Wisen Tanasa <wisen@ceilfors.com>
2018-02-19 07:03:10 +0000

    Simplify unit test. Add missing AWS mock restore.

--- packages/laconia-aws-batch/test/S3ItemReader.spec.js
+++ packages/laconia-aws-batch/test/S3ItemReader.spec.js
@@ -5,83 +5,51 @@ const S3ItemReader = require('../src/S3ItemReader')
 
 const yields = (arg) => (params, callback) => callback(null, arg)
 
-const s3Body = (object) => ({
+const s3Body = (object) => yields({
   Body: {
     toString: () => (JSON.stringify(object))
   }
 })
 
 describe('S3 Item Reader', () => {
-  xdescribe('when path given is an array of 1 item', () => {
-    beforeEach(() => {
-      const s3 = { getObject: jest.fn().mockImplementation(yields(
-        s3Body({
-          music: {
-            list: [
-              {Artist: 'Foo'}
-            ]
-          }
-        })
-      ))}
+  let s3
 
-      AWSMock.mock('S3', 'getObject', s3.getObject)
-    })
-
-    it('should be able to get next items', async () => {
-      const reader = new S3ItemReader(new AWS.S3(), {Bucket: 'bucket', Key: 'key'}, 'music.list')
-      let next = await reader.next()
-
-      expect(next).toEqual({
-        item: {Artist: 'Foo'},
-        cursor: { index: 0 },
-        finished: true
-      })
-    })
+  beforeEach(() => {
+    s3 = { getObject: jest.fn() }
+    AWSMock.mock('S3', 'getObject', s3.getObject)
   })
 
-  describe('when path given is an array of 3 items', () => {
-    beforeEach(() => {
-      const s3 = { getObject: jest.fn().mockImplementation(yields(
-        s3Body({
-          music: {
-            list: [
-              {Artist: 'Foo'},
-              {Artist: 'Bar'},
-              {Artist: 'Fiz'}
-            ]
-          }
-        })
-      ))}
-
-      AWSMock.mock('S3', 'getObject', s3.getObject)
-    })
-
-    it('should be able to get next items', async () => {
-      const reader = new S3ItemReader(new AWS.S3(), {Bucket: 'bucket', Key: 'key'}, 'music.list')
-      let next = await reader.next()
-
-      expect(next).toEqual({
-        item: {Artist: 'Foo'},
-        cursor: { index: 0 },
-        finished: false
-      })
-
-      next = await reader.next(next.cursor)
-      expect(next).toEqual({
-        item: {Artist: 'Bar'},
-        cursor: { index: 1 },
-        finished: false
-      })
-
-      next = await reader.next(next.cursor)
-      expect(next).toEqual({
-        item: {Artist: 'Fiz'},
-        cursor: { index: 2 },
-        finished: true
-      })
-    })
+  afterEach(() => {
+    AWSMock.restore()
   })
 
+  it('retrieves next item when path given is an array of 1 item', async () => {
+    s3.getObject.mockImplementation(s3Body({
+      list: ['Foo']
+    }))
+    const reader = new S3ItemReader(new AWS.S3(), {Bucket: 'bucket', Key: 'key'}, 'list')
+    let next = await reader.next()
+
+    expect(next).toEqual({ item: 'Foo', cursor: { index: 0 }, finished: true })
+  })
+
+  it('retrieves next item when path given is an array of 3 items', async () => {
+    s3.getObject.mockImplementation(s3Body({
+      list: ['Foo', 'Bar', 'Fiz']
+    }))
+
+    const reader = new S3ItemReader(new AWS.S3(), {Bucket: 'bucket', Key: 'key'}, 'list')
+    let next = await reader.next()
+    expect(next).toEqual({ item: 'Foo', cursor: { index: 0 }, finished: false })
+
+    next = await reader.next(next.cursor)
+    expect(next).toEqual({ item: 'Bar', cursor: { index: 1 }, finished: false })
+
+    next = await reader.next(next.cursor)
+    expect(next).toEqual({ item: 'Fiz', cursor: { index: 2 }, finished: true })
+  })
+
+  it('should be able to retrieve a directly stored array')
   it('should be able to handle various paths')
   describe('when path given is not an array', () => {
     it('throw error')

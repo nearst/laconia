fec94e3bbd3e05441149ef7e0b09e18d0851132c
Wisen Tanasa <wisen@ceilfors.com>
2018-02-21 07:02:53 +0000

    Reorganise test and add failing test.

--- packages/laconia-aws-batch/test/S3ItemReader.spec.js
+++ packages/laconia-aws-batch/test/S3ItemReader.spec.js
@@ -2,6 +2,7 @@
 const AWSMock = require("aws-sdk-mock");
 const AWS = require("aws-sdk");
 const S3ItemReader = require("../src/S3ItemReader");
+const _ = require("lodash");
 
 const yields = arg => (params, callback) => callback(null, arg);
 
@@ -42,27 +43,6 @@ describe("S3 Item Reader", () => {
     expect(next).toEqual({ item: "Foo", cursor: { index: 0 }, finished: true });
   });
 
-  it("retrieves next item when path given is an array of 3 items", async () => {
-    s3.getObject.mockImplementation(s3Body(["Foo", "Bar", "Fiz"]));
-    const reader = new S3ItemReader(new AWS.S3(), s3Params, ".");
-    let next = await reader.next();
-    expect(next).toEqual({
-      item: "Foo",
-      cursor: { index: 0 },
-      finished: false
-    });
-
-    next = await reader.next(next.cursor);
-    expect(next).toEqual({
-      item: "Bar",
-      cursor: { index: 1 },
-      finished: false
-    });
-
-    next = await reader.next(next.cursor);
-    expect(next).toEqual({ item: "Fiz", cursor: { index: 2 }, finished: true });
-  });
-
   it("retrieves array from a simple object path", async () => {
     s3.getObject.mockImplementation(
       s3Body({
@@ -99,7 +79,42 @@ describe("S3 Item Reader", () => {
     expect(next).toHaveProperty("item", "Foo");
   });
 
-  it("caches result");
+  describe("when multiple items are returned", () => {
+    let nexts;
+
+    beforeEach(async () => {
+      nexts = [];
+      s3.getObject.mockImplementation(s3Body(["Foo", "Bar", "Fiz"]));
+      const reader = new S3ItemReader(new AWS.S3(), s3Params, ".");
+      nexts.push(await reader.next());
+      nexts.push(await reader.next(_.last(nexts).cursor));
+      nexts.push(await reader.next(_.last(nexts).cursor));
+    });
+
+    it("generates nexts object with the correct content", async () => {
+      expect(nexts[0]).toEqual({
+        item: "Foo",
+        cursor: { index: 0 },
+        finished: false
+      });
+      expect(nexts[1]).toEqual({
+        item: "Bar",
+        cursor: { index: 1 },
+        finished: false
+      });
+      expect(nexts[2]).toEqual({
+        item: "Fiz",
+        cursor: { index: 2 },
+        finished: true
+      });
+    });
+
+    xit("caches result", async () => {
+      expect(s3.getObject).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it("should be able to resume from the middle");
 
   describe("when path given is not an array", () => {
     it("throw error");

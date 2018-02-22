af3f69f7a30b4912c5085763ba02e525c3efe9bb
Wisen Tanasa <wisen@ceilfors.com>
2018-02-22 07:04:59 +0000

    Cache S3 response.

--- packages/laconia-aws-batch/src/S3ItemReader.js
+++ packages/laconia-aws-batch/src/S3ItemReader.js
@@ -1,23 +1,39 @@
 const _ = require('lodash')
 
+const extractS3JsonBody = data => {
+  const fileContent = data.Body.toString()
+  return JSON.parse(fileContent)
+}
+
 module.exports = class S3ItemReader {
   constructor (s3, baseParams, path) {
     this.s3 = s3
     this.baseParams = baseParams
     this.path = path
+    this.cachedItems = []
   }
 
-  async next (cursor = {index: -1}) {
+  async _getItemsFromS3 () {
     const data = await this.s3.getObject(this.baseParams).promise()
-    const fileContent = data.Body.toString()
-    const object = JSON.parse(fileContent)
-    const index = cursor.index + 1
-
+    const object = extractS3JsonBody(data)
     const items = this.path === '.' ? object : _.get(object, this.path)
 
     if (items === undefined) {
       throw new Error(`Path '${this.path}' is not an array, it is undefined`)
     }
+    this.cachedItems = items
+    return items
+  }
+
+  _hasNextItem (items, currentIndex) { return currentIndex >= 0 && currentIndex + 1 <= items.length - 1 }
+
+  _hasNextItemInCache (cursor) { return this._hasNextItem(this.cachedItems, cursor.index) }
+
+  async next (cursor = {index: -1}) {
+    const items = this._hasNextItemInCache(cursor)
+      ? this.cachedItems
+      : await this._getItemsFromS3()
+    const index = cursor.index + 1
 
     return {
       item: items[index],

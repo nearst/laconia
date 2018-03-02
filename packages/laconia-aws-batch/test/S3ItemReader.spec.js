508412b7dd5acaae6cc2d19bc361d26ce7e1c76d
Wisen Tanasa <wisen@ceilfors.com>
2018-03-02 06:38:22 +0000

    Add missing await.

--- packages/laconia-aws-batch/test/S3ItemReader.spec.js
+++ packages/laconia-aws-batch/test/S3ItemReader.spec.js
@@ -153,7 +153,7 @@ describe('S3 Item Reader', () => {
     const oneThousand = _.range(1000)
     s3.getObject.mockImplementation(s3Body(oneThousand))
     const reader = new S3ItemReader(new AWS.S3(), s3Params, '.')
-    oneThousand.reduce(async (cursorPromise, value) => {
+    await oneThousand.reduce(async (cursorPromise, value) => {
       const next = await reader.next(await cursorPromise)
       expect(next).toEqual({ item: value, cursor: { index: value }, finished: value === _.last(oneThousand) })
       return next.cursor

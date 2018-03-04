ef679ab3fb641ff8bba18031a78ca2f9f6a4476d
Wisen Tanasa <wisen@ceilfors.com>
2018-03-04 22:35:49 +0000

    Add unit test for dynamodb batch handler.

---
+++ packages/laconia-aws-batch/test/dynamodb-batch-handler.spec.js
@@ -0,0 +1,35 @@
+/* eslint-env jest */
+
+const AWSMock = require('aws-sdk-mock')
+const dynamodbBatchHandler = require('../src/dynamodb-batch-handler')
+const { sharedBehaviour } = require('./shared-batch-handler-spec')
+const { yields } = require('laconia-test-helper')
+
+describe('dynamodb batch handler', () => {
+  let documentClient
+
+  beforeEach(() => {
+    documentClient = { scan: jest.fn().mockImplementation(yields(
+      {
+        Items: [
+          {Artist: 'Foo'},
+          {Artist: 'Bar'},
+          {Artist: 'Fiz'}
+        ]
+      }
+      ))
+    }
+    AWSMock.mock('DynamoDB.DocumentClient', 'scan', documentClient.scan)
+  })
+
+  afterEach(() => {
+    AWSMock.restore()
+  })
+
+  sharedBehaviour(() => {
+    return dynamodbBatchHandler(
+      'SCAN',
+      { TableName: 'Music' }
+    )
+  })
+})

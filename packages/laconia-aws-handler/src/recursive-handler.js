52f33ad45b8c8a55cf31f3bfb87d147bb170915b
Wisen Tanasa <wisen@ceilfors.com>
2018-03-06 06:30:01 +0000

    Initial implementation of recursive-handler.

---
+++ packages/laconia-aws-handler/src/recursive-handler.js
@@ -0,0 +1,9 @@
+const { lambdaInvoker } = require("@laconia/aws-invoke");
+const basicHandler = require("./basic-handler");
+
+module.exports = handler =>
+  basicHandler((event, context) => {
+    const recurse = response =>
+      lambdaInvoker(context.functionName).fireAndForget(response);
+    return handler(event, context, recurse);
+  });
--- packages/laconia-aws-handler/test/recursive-handler.spec.js
+++ packages/laconia-aws-handler/test/recursive-handler.spec.js
@@ -3,18 +3,53 @@
 const recursiveHandler = require("../src/recursive-handler");
 const AWSMock = require("aws-sdk-mock");
 
-describe("aws recursive handler", () => {
+describe("recursive handler", () => {
   let callback, invokeMock;
 
   beforeEach(() => {
     callback = jest.fn();
-    beforeEach(() => {
-      invokeMock = jest.fn();
-      AWSMock.mock("Lambda", "invoke", invokeMock);
-    });
+    invokeMock = jest.fn();
+    AWSMock.mock("Lambda", "invoke", invokeMock);
   });
 
-  xit("provide recurse callback");
+  afterEach(() => {
+    AWSMock.restore();
+  });
+
+  it("should call Lambda callback with null when there is no value returned", async () => {
+    await recursiveHandler(() => {})({}, {}, callback);
+    expect(callback).toBeCalledWith(null, undefined);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it("should delegate AWS parameters to handler function", async () => {
+    const handler = jest.fn();
+    await recursiveHandler(handler)({ foo: "bar" }, { fiz: "baz" }, callback);
+    expect(handler).toBeCalledWith(
+      { foo: "bar" },
+      { fiz: "baz" },
+      expect.any(Function)
+    );
+  });
+
+  it("should return error to callback when lambda recursion failed");
+
+  it("provides recurse callback", async () => {
+    const context = { functionName: "foo" };
+    const event = {};
+    await recursiveHandler((event, context, recurse) => {
+      recurse("payload");
+    })(event, context, callback);
+
+    expect(invokeMock).toBeCalledWith(
+      expect.objectContaining({
+        FunctionName: "foo",
+        InvocationType: "Event",
+        Payload: JSON.stringify("payload")
+      }),
+      expect.any(Function)
+    );
+  });
 
   xit("should recurse when value returned is not undefined", async () => {
     const context = { functionName: "foo" };
@@ -29,6 +64,8 @@ describe("aws recursive handler", () => {
       expect.any(Function)
     );
   });
+
+  it("should not recurse when value returned is undefined"); // Is this too dangerous for an API? Should we just not implement this?
 
   xit(
     "should merge and take the returned object as precedence to event (should forward event)"

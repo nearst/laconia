7a0101e8caaec6211d62f468cf96bdf93b313724
Wisen Tanasa <wisen@ceilfors.com>
2018-03-08 06:38:30 +0000

    Throw error when payload is not a plain object.

--- packages/laconia-aws-handler/src/recursive-handler.js
+++ packages/laconia-aws-handler/src/recursive-handler.js
@@ -1,9 +1,13 @@
 const { lambdaInvoker } = require('@laconia/aws-invoke')
 const basicHandler = require('./basic-handler')
+const _ = require('lodash')
 
 module.exports = (handler) => basicHandler((event, context) => {
-  const recurse = (response) => {
-    return lambdaInvoker(context.functionName).fireAndForget(Object.assign({}, event, response))
+  const recurse = (payload = {}) => {
+    if (!_.isPlainObject(payload)) {
+      throw new Error('Payload must be an object')
+    }
+    return lambdaInvoker(context.functionName).fireAndForget(Object.assign({}, event, payload))
   }
   return handler(event, context, recurse)
 })

6a5aaf0ce917e49a3d1663bdd2e0d289e625d929
Wisen Tanasa <wisen@ceilfors.com>
2018-03-09 06:53:53 +0000

    Avoid usage of jest mock.calls[0][0].

--- packages/laconia-aws-handler/test/recursive-handler.spec.js
+++ packages/laconia-aws-handler/test/recursive-handler.spec.js
@@ -4,6 +4,15 @@ const { yields } = require("laconia-test-helper");
 const recursiveHandler = require("../src/recursive-handler");
 const AWSMock = require("aws-sdk-mock");
 
+expect.extend({
+  toBeCalledWithPayload(received, expected) {
+    expect(received).toHaveBeenCalledTimes(1);
+    const payload = JSON.parse(received.mock.calls[0][0].Payload);
+    expect(payload).toEqual(expected);
+    return { pass: true };
+  }
+});
+
 describe("recursive handler", () => {
   let context, callback, invokeMock;
 
@@ -70,8 +79,11 @@ describe("recursive handler", () => {
       callback
     );
     expect(callback).toBeCalledWith(expect.any(Error));
-    const error = callback.mock.calls[0][0];
-    expect(error.message).toContain("Payload must be an object");
+    expect(callback).toBeCalledWith(
+      expect.objectContaining({
+        message: expect.stringContaining("Payload must be an object")
+      })
+    );
   });
 
   it("should merge recurse payload and event object", async () => {
@@ -79,9 +91,7 @@ describe("recursive handler", () => {
       recurse({ cursor: { index: 0, lastEvaluatedKey: "bar" } });
     })({ key1: "1", key2: "2" }, context, callback);
 
-    expect(invokeMock).toHaveBeenCalledTimes(1);
-    const payload = JSON.parse(invokeMock.mock.calls[0][0].Payload);
-    expect(payload).toEqual({
+    expect(invokeMock).toBeCalledWithPayload({
       key1: "1",
       key2: "2",
       cursor: { index: 0, lastEvaluatedKey: "bar" }

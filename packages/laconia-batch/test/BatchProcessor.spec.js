d1d74bf4946fe421245c69a2a53aa07741ad903d
Wisen Tanasa <wisen@ceilfors.com>
2018-03-16 06:36:14 +0000

    DRY spec introducing array reader.

--- packages/laconia-batch/test/BatchProcessor.spec.js
+++ packages/laconia-batch/test/BatchProcessor.spec.js
@@ -2,6 +2,14 @@ const BatchProcessor = require("../src/BatchProcessor");
 const { matchers } = require("laconia-test-helper");
 expect.extend(matchers);
 
+const arrayReader = array =>
+  array.reduce((reader, item, index) => {
+    const finished = index === array.length - 1;
+    return reader.mockImplementationOnce(() =>
+      Promise.resolve({ item, finished })
+    );
+  }, jest.fn());
+
 describe("BatchProcessor", () => {
   let itemListener;
   let shouldStop;
@@ -25,17 +33,7 @@ describe("BatchProcessor", () => {
   describe("when multiple items were returned", () => {
     beforeEach(() => {
       const batchProcessor = new BatchProcessor(
-        jest
-          .fn()
-          .mockImplementationOnce(() =>
-            Promise.resolve({ item: "1", finished: false })
-          )
-          .mockImplementationOnce(() =>
-            Promise.resolve({ item: "2", finished: false })
-          )
-          .mockImplementationOnce(() =>
-            Promise.resolve({ item: "3", finished: true })
-          ),
+        arrayReader(["1", "2", "3"]),
         shouldStop
       ).on("item", itemListener);
       return batchProcessor.start();
@@ -53,27 +51,23 @@ describe("BatchProcessor", () => {
   });
 
   describe("when rate limited", () => {
+    const createBatchProcessor = itemsPerSecond =>
+      new BatchProcessor(arrayReader(["1", "2", "3"]), shouldStop, {
+        itemsPerSecond
+      }).on("item", itemListener);
+
     it("should process items with delay", async () => {
-      const batchProcessor = new BatchProcessor(
-        jest
-          .fn()
-          .mockImplementationOnce(() =>
-            Promise.resolve({ item: "1", finished: false })
-          )
-          .mockImplementationOnce(() =>
-            Promise.resolve({ item: "2", finished: false })
-          )
-          .mockImplementationOnce(() =>
-            Promise.resolve({ item: "3", finished: true })
-          ),
-        shouldStop,
-        {
-          itemsPerSecond: 10
-        }
-      ).on("item", itemListener);
+      const batchProcessor = createBatchProcessor(10);
       await batchProcessor.start();
 
       expect(itemListener).toBeCalledWithGapBetween(5, 110);
+    });
+
+    it("should support 1000 itemsPerSecond", async () => {
+      const batchProcessor = createBatchProcessor(1000);
+      await batchProcessor.start();
+
+      expect(itemListener).toBeCalledWithGapBetween(0, 10);
     });
   });
 });

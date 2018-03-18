31cf55e5ef3eb169aaebffa9b8929cc9ba9da096
Wisen Tanasa <wisen@ceilfors.com>
2018-03-18 00:26:19 +0000

    Add acceptance tests for laconia-handlers.

---
+++ packages/laconia-acceptance-test/test/acceptance.spec.js
@@ -0,0 +1,22 @@
+const { lambdaInvoker } = require('laconia-invoke')
+const tracker = require('laconia-test-helper').tracker
+
+const prefix = 'laconia-acceptance-test'
+const name = name => `${prefix}-${name}`
+
+describe('laconia-handler basicHandler', () => {
+  it('returns result', async () => {
+    const result = await lambdaInvoker(name('handler-basic')).requestResponse()
+    expect(result).toEqual('hello')
+  })
+})
+
+describe('laconia-handler recursiveHandler', () => {
+  it('recurses three times', async () => {
+    const recursiveTracker = tracker('recursive', name('tracker'))
+    await recursiveTracker.clear()
+    await lambdaInvoker(name('handler-recursive')).fireAndForget({input: 1})
+
+    await recursiveTracker.waitUntil(3)
+  })
+})

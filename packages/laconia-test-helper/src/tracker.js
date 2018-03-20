55c9b4f745cec75c0dca90752d8a60d37ad796fa
Wisen Tanasa <wisen@ceilfors.com>
2018-03-20 06:45:20 +0000

    Green acceptance test for s3 batch.
    - Reorganise acceptance test src
    - Reimplement tracker to use timestamp as a key as listObject's
       consistency is eventual. Because of this, s3.waitFor is now unusable
       and we are using p-wait-for instead.

--- packages/laconia-test-helper/src/tracker.js
+++ packages/laconia-test-helper/src/tracker.js
@@ -1,4 +1,5 @@
 const AWS = require("aws-sdk");
+const pWaitFor = require("p-wait-for");
 const s3 = new AWS.S3();
 
 const getBucket = bucketName => {
@@ -15,24 +16,39 @@ module.exports = (id, bucketName) => {
   const bucket = getBucket(bucketName);
 
   return {
-    async tick() {
-      const total = await this.getTotal();
+    async tick(info) {
       return s3
         .putObject({
           Bucket: bucket,
-          Key: `${id}/${total + 1}`
+          Key: `${id}/${Date.now()}`,
+          Body: JSON.stringify(info)
         })
         .promise();
     },
 
     waitUntil(totalTicks) {
-      return s3
-        .waitFor("objectExists", {
+      return pWaitFor(async () => (await this.getTotal()) >= totalTicks);
+    },
+
+    async getTicks() {
+      const objects = await s3
+        .listObjects({
           Bucket: bucket,
-          Key: `${id}/${totalTicks}`,
-          $waiter: { delay: 1 }
+          Prefix: id
         })
         .promise();
+      return Promise.all(
+        objects.Contents.map(t =>
+          s3
+            .getObject({
+              Bucket: bucket,
+              Key: t.Key
+            })
+            .promise()
+        )
+      ).then(results => {
+        return results.map(result => JSON.parse(result.Body.toString()));
+      });
     },
 
     async getTotal() {
@@ -42,7 +58,7 @@ module.exports = (id, bucketName) => {
           Prefix: id
         })
         .promise();
-      return objects.Contents.length;
+      return Number(objects.Contents.length);
     },
 
     async clear() {

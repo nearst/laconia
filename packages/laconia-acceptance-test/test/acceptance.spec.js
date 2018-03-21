be7a32a6080298a353df8a1c1a2f53697fe22ee0
Wisen Tanasa <wisen@ceilfors.com>
2018-03-21 06:41:41 +0000

    Refactor s3 acceptance test.

--- packages/laconia-acceptance-test/test/acceptance.spec.js
+++ packages/laconia-acceptance-test/test/acceptance.spec.js
@@ -6,6 +6,10 @@ const name = name => `${prefix}-${name}`
 const AWS = require('aws-sdk')
 const s3 = new AWS.S3()
 
+const items = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+jest.setTimeout(10000)
+
 describe('laconia-handler basicHandler', () => {
   it('returns result', async () => {
     const result = await lambdaInvoker(name('handler-basic')).requestResponse()
@@ -14,42 +18,43 @@ describe('laconia-handler basicHandler', () => {
 })
 
 describe('laconia-handler recursiveHandler', () => {
-  it('recurses three times', async () => {
-    const recursiveTracker = tracker('recursive', name('tracker'))
+  const recursiveTracker = tracker('recursive', name('tracker'))
+
+  beforeAll(async () => {
     await recursiveTracker.clear()
     await lambdaInvoker(name('handler-recursive')).fireAndForget({input: 1})
-
     await recursiveTracker.waitUntil(3)
+  })
+
+  it('recurses three times', async () => {
     expect(await recursiveTracker.getTotal()).toEqual(3)
   })
 })
 
 describe('laconia-batch-s3 s3-batch-handler', () => {
-  it('processes all items with at least 2 different Lambda request Id', async () => {
-    jest.setTimeout(10000)
-    const s3BatchTracker = tracker('batch-s3', name('tracker'))
-    await s3BatchTracker.clear()
-    const items = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-    await s3.putObject({
-      Bucket: name('bucket'),
-      Key: 'batch-s3.json',
-      Body: JSON.stringify(items)
-    }).promise()
-    await lambdaInvoker(name('batch-s3')).fireAndForget()
+  const s3BatchTracker = tracker('batch-s3', name('tracker'))
 
+  beforeAll(() => s3.putObject({
+    Bucket: name('bucket'),
+    Key: 'batch-s3.json',
+    Body: JSON.stringify(items)
+  }).promise())
+
+  beforeAll(async () => {
+    await s3BatchTracker.clear()
+    await lambdaInvoker(name('batch-s3')).fireAndForget()
     await s3BatchTracker.waitUntil(10)
+  })
+
+  it('processes items with at least 3 separate Lambda requests', async () => {
     const ticks = await s3BatchTracker.getTicks()
     const requestIds = ticks.map(t => t.context.awsRequestId)
-    expect(requestIds.length).toBeGreaterThanOrEqual(2)
-
-    const tickItems = ticks.map(t => t.item)
-    expect(tickItems).toHaveLength(10)
-    items.forEach((item) => {
-      expect(tickItems).toContain(item)
-    })
+    expect(requestIds.length).toBeGreaterThanOrEqual(3)
   })
-})
 
-describe('laconia-dynamodb-batch-handler', () => {
-  it('processes all items with at least 3 different Lambda request Id')
+  it('processes all items', async () => {
+    const ticks = await s3BatchTracker.getTicks()
+    const tickItems = ticks.map(t => t.item)
+    expect(tickItems).toEqual(items)
+  })
 })

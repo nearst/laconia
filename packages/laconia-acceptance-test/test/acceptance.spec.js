da9a1ff65b8d78f4a5fa2775a0872a127e3d6e7b
Wisen Tanasa <wisen@ceilfors.com>
2018-03-22 06:50:45 +0000

    Introduce dynamodb acceptance test.
    Reorganise acceptance test to be in one it block
    as beforeAll() is always executed regardless
    of a focused described block.

--- packages/laconia-acceptance-test/test/acceptance.spec.js
+++ packages/laconia-acceptance-test/test/acceptance.spec.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const { lambdaInvoker } = require("laconia-invoke");
 const tracker = require("laconia-test-helper").tracker;
 
@@ -5,8 +6,10 @@ const prefix = "laconia-acceptance-test";
 const name = name => `${prefix}-${name}`;
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
+const documentClient = new AWS.DynamoDB.DocumentClient();
 
 const items = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+const getRequestIds = ticks => _.uniq(ticks.map(t => t.context.awsRequestId));
 
 jest.setTimeout(10000);
 
@@ -20,18 +23,16 @@ describe("laconia-handler basicHandler", () => {
 describe("laconia-handler recursiveHandler", () => {
   const recursiveTracker = tracker("recursive", name("tracker"));
 
-  beforeAll(async () => {
-    await recursiveTracker.clear();
-    await lambdaInvoker(name("handler-recursive")).fireAndForget({ input: 1 });
-    await recursiveTracker.waitUntil(3);
-  });
+  beforeAll(() => recursiveTracker.clear());
 
   it("recurses three times", async () => {
+    await lambdaInvoker(name("handler-recursive")).fireAndForget({ input: 1 });
+    await recursiveTracker.waitUntil(3);
     expect(await recursiveTracker.getTotal()).toEqual(3);
   });
 });
 
-describe("laconia-batch-s3 s3-batch-handler", () => {
+describe("laconia-batch s3-batch-handler", () => {
   const s3BatchTracker = tracker("batch-s3", name("tracker"));
 
   beforeAll(() =>
@@ -43,21 +44,44 @@ describe("laconia-batch-s3 s3-batch-handler", () => {
       })
       .promise());
 
-  beforeAll(async () => {
-    await s3BatchTracker.clear();
-    await lambdaInvoker(name("batch-s3")).fireAndForget();
-    await s3BatchTracker.waitUntil(10);
-  });
+  beforeAll(() => s3BatchTracker.clear());
 
-  it("processes items with at least 3 separate Lambda requests", async () => {
-    const ticks = await s3BatchTracker.getTicks();
-    const requestIds = ticks.map(t => t.context.awsRequestId);
-    expect(requestIds.length).toBeGreaterThanOrEqual(3);
-  });
+  const getItems = ticks => ticks.map(t => t.item);
 
   it("processes all items", async () => {
+    await lambdaInvoker(name("batch-s3")).fireAndForget();
+    await s3BatchTracker.waitUntil(10);
+
     const ticks = await s3BatchTracker.getTicks();
-    const tickItems = ticks.map(t => t.item);
-    expect(tickItems).toEqual(items);
+    expect(getRequestIds(ticks).length).toBeGreaterThanOrEqual(3);
+    expect(getItems(ticks)).toEqual(items);
+  });
+});
+
+describe("laconia-batch dynamodb-batch-handler", () => {
+  const dynamoDbBatchTracker = tracker("batch-dynamoDb", name("tracker"));
+
+  beforeAll(() =>
+    documentClient
+      .batchWrite({
+        RequestItems: {
+          [name("batch")]: items.map(i =>
+            _.set({}, "PutRequest.Item.ArtistId", i)
+          )
+        }
+      })
+      .promise());
+
+  beforeAll(() => dynamoDbBatchTracker.clear());
+
+  const getArtistIds = ticks => ticks.map(t => t.item.ArtistId).sort();
+
+  it("processes all items", async () => {
+    await lambdaInvoker(name("batch-dynamodb")).fireAndForget();
+    await dynamoDbBatchTracker.waitUntil(10);
+
+    const ticks = await dynamoDbBatchTracker.getTicks();
+    expect(getRequestIds(ticks).length).toBeGreaterThanOrEqual(3);
+    expect(getArtistIds(ticks)).toEqual(items);
   });
 });

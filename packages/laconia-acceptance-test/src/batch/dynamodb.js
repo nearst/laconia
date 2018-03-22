1e421d08a80be65a190f186230a460513b9f68f2
Wisen Tanasa <wisen@ceilfors.com>
2018-03-22 06:50:45 +0000

    Introduce dynamodb acceptance test.
    Reorganise acceptance test to be in one it block
    as beforeAll() is always executed regardless
    of a focused described block.

---
+++ packages/laconia-acceptance-test/src/batch/dynamodb.js
@@ -0,0 +1,17 @@
+const { dynamoDbBatchHandler } = require('laconia-batch')
+const tracker = require('laconia-test-helper').tracker('batch-dynamoDb')
+
+let lambdaContext
+module.exports.handler = dynamoDbBatchHandler(
+  'SCAN',
+  { TableName: process.env['TABLE_NAME'] },
+  { itemsPerSecond: 2 }
+)
+.on('start', (event, context) => { lambdaContext = context })
+.on('item', item => {
+  console.log(item)
+  return tracker.tick({
+    context: lambdaContext,
+    item
+  })
+})

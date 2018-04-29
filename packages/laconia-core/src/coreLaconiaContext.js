42d6d2c15272cc4837ffcfec628d556ab27811ca
Wisen Tanasa <wisen@ceilfors.com>
2018-04-29 09:49:22 +0100

    Add custom injection support in laconiaContext.

--- packages/laconia-core/src/coreLaconiaContext.js
+++ packages/laconia-core/src/coreLaconiaContext.js
@@ -1,11 +1,28 @@
 const invoke = require("./invoke");
 const recurse = require("./recurse");
 
+const prefixKeys = (prefix, object) => {
+  const keyValues = Object.keys(object).map(key => {
+    const newKey = `${prefix}${key}`;
+    return { [newKey]: object[key] };
+  });
+  return Object.assign({}, ...keyValues);
+};
+
 module.exports = laconiaContext => {
-  const coreLaconiaContext = {
+  const coreMembers = {
     invoke,
     recurse: recurse(laconiaContext),
     env: process.env
   };
-  return Object.assign({}, laconiaContext, coreLaconiaContext);
+  const coreLaconiaContext = Object.assign(
+    {},
+    laconiaContext,
+    coreMembers,
+    prefixKeys("$", coreMembers)
+  );
+  coreLaconiaContext.inject = member => {
+    return Object.assign(coreLaconiaContext, member);
+  };
+  return coreLaconiaContext;
 };
--- packages/laconia-core/test/coreLaconiaContext.spec.js
+++ packages/laconia-core/test/coreLaconiaContext.spec.js
@@ -3,19 +3,40 @@ const invoke = require("../src/invoke");
 
 describe("laconiaContext", () => {
   it("should include invoke function", () => {
-    const result = laconiaContext({ foo: "event" }, { fiz: "context" });
-    expect(result).toEqual(expect.objectContaining({ invoke }));
+    const lc = laconiaContext({});
+    expect(lc).toEqual(expect.objectContaining({ invoke }));
   });
 
   it("should include instantiated recurse function", () => {
-    const result = laconiaContext({ foo: "event" }, { fiz: "context" });
-    expect(result).toEqual(
+    const lc = laconiaContext({});
+    expect(lc).toEqual(
       expect.objectContaining({ recurse: expect.any(Function) })
     );
   });
 
   it("should include process.env", () => {
-    const result = laconiaContext({ foo: "event" }, { fiz: "context" });
-    expect(result).toEqual(expect.objectContaining({ env: process.env }));
+    const lc = laconiaContext({});
+    expect(lc).toEqual(expect.objectContaining({ env: process.env }));
+  });
+
+  it("should be able to inject new member", () => {
+    const lc = laconiaContext({});
+    lc.inject({ foo: "bar" });
+    expect(lc).toEqual(expect.objectContaining({ foo: "bar" }));
+    lc.inject({ boo: "baz" });
+    expect(lc).toEqual(expect.objectContaining({ foo: "bar" }));
+    expect(lc).toEqual(expect.objectContaining({ boo: "baz" }));
+  });
+
+  it("should be able to override member", () => {
+    const lc = laconiaContext({});
+    lc.inject({ env: "bar" });
+    expect(lc).toEqual(expect.objectContaining({ env: "bar" }));
+  });
+
+  it("should make built-in members available after being overridden", () => {
+    const lc = laconiaContext({});
+    lc.inject({ env: "bar" });
+    expect(lc).toEqual(expect.objectContaining({ $env: process.env }));
   });
 });

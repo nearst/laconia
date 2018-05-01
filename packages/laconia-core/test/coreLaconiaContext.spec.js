7ec33d02be22660ad7aa44383885d790a3a4f79b
Wisen Tanasa <wisen@ceilfors.com>
2018-05-01 06:40:22 +0100

    Refactor spec to use toHaveProperty.

--- packages/laconia-core/test/coreLaconiaContext.spec.js
+++ packages/laconia-core/test/coreLaconiaContext.spec.js
@@ -4,39 +4,37 @@ const invoke = require("../src/invoke");
 describe("laconiaContext", () => {
   it("should include invoke function", () => {
     const lc = laconiaContext({});
-    expect(lc).toEqual(expect.objectContaining({ invoke }));
+    expect(lc).toHaveProperty("invoke", invoke);
   });
 
   it("should include instantiated recurse function", () => {
     const lc = laconiaContext({});
-    expect(lc).toEqual(
-      expect.objectContaining({ recurse: expect.any(Function) })
-    );
+    expect(lc).toHaveProperty("recurse", expect.any(Function));
   });
 
   it("should include process.env", () => {
     const lc = laconiaContext({});
-    expect(lc).toEqual(expect.objectContaining({ env: process.env }));
+    expect(lc).toHaveProperty("env", process.env);
   });
 
   it("should be able to inject new member", () => {
     const lc = laconiaContext({});
     lc.inject({ foo: "bar" });
-    expect(lc).toEqual(expect.objectContaining({ foo: "bar" }));
+    expect(lc).toHaveProperty("foo", "bar");
     lc.inject({ boo: "baz" });
-    expect(lc).toEqual(expect.objectContaining({ foo: "bar" }));
-    expect(lc).toEqual(expect.objectContaining({ boo: "baz" }));
+    expect(lc).toHaveProperty("foo", "bar");
+    expect(lc).toHaveProperty("boo", "baz");
   });
 
   it("should be able to override member", () => {
     const lc = laconiaContext({});
     lc.inject({ env: "bar" });
-    expect(lc).toEqual(expect.objectContaining({ env: "bar" }));
+    expect(lc).toHaveProperty("env", "bar");
   });
 
   it("should make built-in members available after being overridden", () => {
     const lc = laconiaContext({});
     lc.inject({ env: "bar" });
-    expect(lc).toEqual(expect.objectContaining({ $env: process.env }));
+    expect(lc).toHaveProperty("$env", process.env);
   });
 });

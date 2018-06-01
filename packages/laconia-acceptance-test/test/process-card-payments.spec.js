6a9b91d923ac5a16645729ceec75aef220cafe57
Wisen Tanasa <wisen@ceilfors.com>
2018-06-01 07:25:26 +0100

    Add unit test for process card payment.

---
+++ packages/laconia-acceptance-test/test/process-card-payments.spec.js
@@ -0,0 +1,11 @@
+const handler = require("../src/process-card-payments").handler;
+
+describe("process-card-payments", () => {
+  it("triggers capture card payment lambda on item event", async () => {
+    const captureCardPayment = { fireAndForget: jest.fn() };
+    const item = { paymentReference: "something" };
+    handler.emit("item", { captureCardPayment }, item);
+
+    expect(captureCardPayment.fireAndForget).toBeCalledWith("something");
+  });
+});

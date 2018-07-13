a36cc60f6ba56b3b49fae7a28abefdad716fc1f9
Wisen Tanasa <wisen@ceilfors.com>
2018-07-13 07:26:55 +0100

    Initial experiment to extend error stack.

--- packages/laconia-core/src/InvokeLaconiaError.js
+++ packages/laconia-core/src/InvokeLaconiaError.js
@@ -2,5 +2,22 @@ module.exports = class InvokeLaconiaError extends Error {
   constructor(lambdaErrorPayload) {
     super(lambdaErrorPayload.errorMessage);
     this.name = lambdaErrorPayload.errorType;
+    this.lambdaStackTrace = lambdaErrorPayload.stackTrace;
+
+    Object.defineProperty(this, "stack", {
+      get: () => {
+        const extendedStack =
+          this.invokeError.stack +
+          "\n" +
+          "Caused by Lambda Invocation error:\n" +
+          this.lambdaStackTrace.map(t => `    at ${t}`).join("\n");
+        return extendedStack;
+      },
+      set: value => {}
+    });
+
+    const err = new Error();
+    Error.captureStackTrace(err, InvokeLaconiaError);
+    this.invokeError = err;
   }
 };

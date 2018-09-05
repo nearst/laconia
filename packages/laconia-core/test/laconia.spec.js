2164f7f7fefd63cabfab535132ccc6dde17b2e24
Wisen Tanasa <wisen@ceilfors.com>
2018-09-05 06:48:04 +0100

    Organise laconia.spec

--- packages/laconia-core/test/laconia.spec.js
+++ packages/laconia-core/test/laconia.spec.js
@@ -46,63 +46,66 @@ describe("handler", () => {
     });
   });
 
-  it("should be able to add instances by calling 'register'", async () => {
-    const handler = jest.fn();
-    await laconia(handler)
-      .register(lc => ({ foo: "bar" }))
-      .register(lc => ({ boo: "baz" }))(
-      { foo: "event" },
-      { fiz: "context" },
-      callback
-    );
+  describe("#register", () => {
+    it("should be able to add instances by calling 'register'", async () => {
+      const handler = jest.fn();
+      await laconia(handler)
+        .register(lc => ({ foo: "bar" }))
+        .register(lc => ({ boo: "baz" }))(
+        { foo: "event" },
+        { fiz: "context" },
+        callback
+      );
 
-    expect(handler).toBeCalledWith(
-      expect.objectContaining({
-        foo: "bar",
-        boo: "baz"
-      })
-    );
-  });
-
-  it("should be able to add async instances by calling 'register'", async () => {
-    const handler = jest.fn();
-    await laconia(handler).register(async lc => {
-      const instance = await Promise.resolve({ foo: "bar" });
-      return instance;
-    })({ foo: "event" }, { fiz: "context" }, callback);
-
-    expect(handler).toBeCalledWith(
-      expect.objectContaining({
-        foo: "bar"
-      })
-    );
-  });
-
-  describe("when synchronous code is returned", () => {
-    it("should call Lambda callback with the handler return value to Lambda callback", async () => {
-      await laconia(() => "value")({}, {}, callback);
-      expect(callback).toBeCalledWith(null, "value");
+      expect(handler).toBeCalledWith(
+        expect.objectContaining({
+          foo: "bar",
+          boo: "baz"
+        })
+      );
     });
 
-    it("should call Lambda callback with the error thrown", async () => {
-      const error = new Error("boom");
-      await laconia(() => {
-        throw error;
-      })({}, {}, callback);
-      expect(callback).toBeCalledWith(error);
+    it("should be able to add async instances by calling 'register'", async () => {
+      const handler = jest.fn();
+      await laconia(handler).register(async lc => {
+        return Promise.resolve({ foo: "bar" });
+      })({ foo: "event" }, { fiz: "context" }, callback);
+
+      expect(handler).toBeCalledWith(
+        expect.objectContaining({
+          foo: "bar"
+        })
+      );
     });
   });
 
-  describe("when promise is returned", () => {
-    it("should call Lambda callback with the handler return value to Lambda callback", async () => {
-      await laconia(() => Promise.resolve("value"))({}, {}, callback);
-      expect(callback).toBeCalledWith(null, "value");
+  describe("callback behaviour", () => {
+    describe("when synchronous code is returned", () => {
+      it("should call Lambda callback with the handler return value to Lambda callback", async () => {
+        await laconia(() => "value")({}, {}, callback);
+        expect(callback).toBeCalledWith(null, "value");
+      });
+
+      it("should call Lambda callback with the error thrown", async () => {
+        const error = new Error("boom");
+        await laconia(() => {
+          throw error;
+        })({}, {}, callback);
+        expect(callback).toBeCalledWith(error);
+      });
     });
 
-    it("should call Lambda callback with the error thrown", async () => {
-      const error = new Error("boom");
-      await laconia(() => Promise.reject(error))({}, {}, callback);
-      expect(callback).toBeCalledWith(error);
+    describe("when promise is returned", () => {
+      it("should call Lambda callback with the handler return value to Lambda callback", async () => {
+        await laconia(() => Promise.resolve("value"))({}, {}, callback);
+        expect(callback).toBeCalledWith(null, "value");
+      });
+
+      it("should call Lambda callback with the error thrown", async () => {
+        const error = new Error("boom");
+        await laconia(() => Promise.reject(error))({}, {}, callback);
+        expect(callback).toBeCalledWith(error);
+      });
     });
   });
 });

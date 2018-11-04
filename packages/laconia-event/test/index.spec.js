4e0d96c30dbf26fb85ff93e04168be832dd95737
Wisen Tanasa <wisen@ceilfors.com>
2018-11-04 08:39:51 +0000

    #6 Add missing test coverage

--- packages/laconia-event/test/index.spec.js
+++ packages/laconia-event/test/index.spec.js
@@ -1,9 +1,14 @@
-const config = require("../src/index");
+const event = require("../src/index");
 
 describe("@laconia/event", () => {
   describe("#s3", () => {
     it("has s3 function", () => {
-      expect(config.s3).toBeFunction();
+      expect(event.s3).toBeFunction();
+    });
+
+    it("returns an instance of inputConverter", () => {
+      const instances = event.s3()();
+      expect(instances).toHaveProperty("$inputConverter");
     });
   });
 });

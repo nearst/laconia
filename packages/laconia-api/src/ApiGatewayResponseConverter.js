073f955dbea2520920a7b2893b9a730643209d0c
Wisen Tanasa <wisen@ceilfors.com>
2019-01-09 06:56:34 +0000

    GH-13 Support string conversion

--- packages/laconia-api/src/ApiGatewayResponseConverter.js
+++ packages/laconia-api/src/ApiGatewayResponseConverter.js
@@ -1,5 +1,11 @@
 module.exports = class ApiGatewayResponseConverter {
   convert(output) {
+    if (typeof output === "string") {
+      return {
+        body: output,
+        headers: { "Content-Type": "text/plain" }
+      };
+    }
     return {
       body: JSON.stringify(output),
       headers: { "Content-Type": "application/json; charset=utf-8" }
--- packages/laconia-api/test/ApiGatewayResponseConverter.spec.js
+++ packages/laconia-api/test/ApiGatewayResponseConverter.spec.js
@@ -7,19 +7,39 @@ describe("ApiGatewayResponseConverter", () => {
     responseConverter = new ApiGatewayResponseConverter();
   });
 
-  it("should JSON stringify when converting an object", async () => {
-    const input = await responseConverter.convert({ foo: "bar" });
-    expect(input).toEqual(expect.objectContaining({ body: '{"foo":"bar"}' }));
+  describe("when converting an object", () => {
+    it("should set body to the object's JSON string", async () => {
+      const input = await responseConverter.convert({ foo: "bar" });
+      expect(input).toEqual(expect.objectContaining({ body: '{"foo":"bar"}' }));
+    });
+
+    it("should set Content-Type header to application/json", async () => {
+      const input = await responseConverter.convert({ foo: "bar" });
+      expect(input).toEqual(
+        expect.objectContaining({
+          headers: expect.objectContaining({
+            "Content-Type": "application/json; charset=utf-8"
+          })
+        })
+      );
+    });
   });
 
-  it("should return Content-Type of application/json when converting an object", async () => {
-    const input = await responseConverter.convert({ foo: "bar" });
-    expect(input).toEqual(
-      expect.objectContaining({
-        headers: expect.objectContaining({
-          "Content-Type": "application/json; charset=utf-8"
+  describe("when converting a string", () => {
+    it("should set body to the output string", async () => {
+      const input = await responseConverter.convert("foo");
+      expect(input).toEqual(expect.objectContaining({ body: "foo" }));
+    });
+
+    it("should set Content-Type header to text/plain", async () => {
+      const input = await responseConverter.convert("foo");
+      expect(input).toEqual(
+        expect.objectContaining({
+          headers: expect.objectContaining({
+            "Content-Type": "text/plain"
+          })
         })
-      })
-    );
+      );
+    });
   });
 });

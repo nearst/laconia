3ed479835cecb1d8149f8ebd4b6a141faf8b1c4f
Lee Coomber <lee.coomber@sainsburys.co.uk>
2019-05-15 21:15:34 +0100

    Remove original headers and associated test

--- packages/laconia-event/src/apigateway/ApiGatewayInputHeaders.js
+++ packages/laconia-event/src/apigateway/ApiGatewayInputHeaders.js
@@ -1,6 +1,5 @@
 module.exports = class ApiGatewayInputHeaders {
   constructor(eventHeaders) {
-    Object.assign(this, eventHeaders);
     Object.entries(eventHeaders).forEach(([headerName, headerValue]) => {
       this[headerName.toLowerCase()] = headerValue;
     });

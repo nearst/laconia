21bd65d066bc29d6cb008beb69a2f2ceffa12a0e
Wisen Tanasa <wisen@ceilfors.com>
2019-08-20 06:05:07 +0100

    GH-173 Add LaconiaBatchOptions

--- packages/laconia-batch/src/index.d.ts
+++ packages/laconia-batch/src/index.d.ts
@@ -1,9 +1,17 @@
 import { LaconiaHandler } from "@laconia/core";
 
+declare type LaconiaBatchOptions = {
+  itemsPerSecond?: number;
+  timeNeededToRecurseInMillis?: number;
+};
+
 declare namespace laconiaBatch {
   function dynamoDb(): any;
 }
 
-declare function laconiaBatch(app: Function): LaconiaHandler;
+declare function laconiaBatch(
+  app: Function,
+  options: LaconiaBatchOptions
+): LaconiaHandler;
 
 export = laconiaBatch;
--- packages/laconia-batch/test/types/index.ts
+++ packages/laconia-batch/test/types/index.ts
@@ -1,9 +1,6 @@
 import laconiaBatch from "../../src/index";
 
-laconiaBatch(() => laconiaBatch.dynamoDb());
-
-// Customise batch options
-// exports.handler = laconiaBatch(_ => laconiaBatch.dynamoDb(), {
-//   itemsPerSecond: 2,
-//   timeNeededToRecurseInMillis: 10000
-// });
+laconiaBatch(() => laconiaBatch.dynamoDb(), {
+  itemsPerSecond: 2,
+  timeNeededToRecurseInMillis: 10000
+});

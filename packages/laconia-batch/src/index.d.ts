7f36fa47f91ac1def0071ec4bb6b947ff77e48b2
Wisen Tanasa <wisen@ceilfors.com>
2019-08-21 06:35:27 +0100

    GH-173 Add types for reader options

--- packages/laconia-batch/src/index.d.ts
+++ packages/laconia-batch/src/index.d.ts
@@ -5,13 +5,26 @@ declare type LaconiaBatchOptions = {
   timeNeededToRecurseInMillis?: number;
 };
 
+declare type DynamoDbReaderOptions = {
+  operation: "SCAN" | "QUERY";
+  dynamoDbParams: any;
+  documentClient?: any;
+};
+
+declare type S3ReaderOptions = {
+  path: string;
+  s3Params: any;
+  s3?: any;
+};
+
 declare namespace laconiaBatch {
-  function dynamoDb(): any;
+  function dynamoDb(options: DynamoDbReaderOptions): any;
+  function s3(options: S3ReaderOptions): any;
 }
 
 declare function laconiaBatch(
   app: Function,
-  options: LaconiaBatchOptions
+  options?: LaconiaBatchOptions
 ): LaconiaHandler;
 
 export = laconiaBatch;
--- packages/laconia-batch/test/types/index.ts
+++ packages/laconia-batch/test/types/index.ts
@@ -1,6 +1,39 @@
 import laconiaBatch from "../../src/index";
+import AWS from "aws-sdk";
 
-laconiaBatch(() => laconiaBatch.dynamoDb(), {
-  itemsPerSecond: 2,
-  timeNeededToRecurseInMillis: 10000
+laconiaBatch(
+  () =>
+    laconiaBatch.dynamoDb({
+      operation: "SCAN",
+      dynamoDbParams: { TableName: "Music" }
+    }),
+  {
+    itemsPerSecond: 0.5,
+    timeNeededToRecurseInMillis: 10000
+  }
+);
+
+laconiaBatch.dynamoDb({
+  operation: "QUERY",
+  dynamoDbParams: { TableName: "Music" },
+  documentClient: new AWS.DynamoDB.DocumentClient()
+});
+
+laconiaBatch(() =>
+  laconiaBatch.s3({
+    path: ".",
+    s3Params: {
+      Bucket: "MyBucket",
+      Key: "array.json"
+    }
+  })
+).register(() => ({ someKey: "value" }));
+
+laconiaBatch.s3({
+  path: 'database.music[0]["category"].list',
+  s3Params: {
+    Bucket: "MyBucket",
+    Key: "object.json"
+  },
+  s3: new AWS.S3()
 });

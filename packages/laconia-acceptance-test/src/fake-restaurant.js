0e139c694d77c0de134240c0ac6324e269fe09b9
Wisen Tanasa <wisen@ceilfors.com>
2019-08-29 06:42:20 +0100

    Not attempt to parse body when its null
    - Switched from r2 to axios as r2 is failing silently
    - I still have no idea why this is suddenly happened

--- packages/laconia-acceptance-test/src/fake-restaurant.js
+++ packages/laconia-acceptance-test/src/fake-restaurant.js
@@ -1,14 +1,21 @@
 // @ts-check
 const laconia = require("@laconia/core");
 const sns = require("@laconia/adapter").sns();
-const r2 = require("r2");
+const axios = require("axios").default;
 
 const app = async (order, { env }) => {
   console.log("order", order);
   if (order.restaurantId === 1) {
-    console.log(`Accepting order: ${JSON.stringify(order)}`);
     const acceptUrl = `${env.API_BASE_URL}/order/${order.orderId}/accept`;
-    await r2.put(acceptUrl);
+    console.log(`Accepting order ${JSON.stringify(order)} at ${acceptUrl}`);
+    try {
+      await axios.put(acceptUrl);
+    } catch (e) {
+      if (e.response) {
+        console.error("Error from server", e.response.data);
+      }
+      throw e;
+    }
   }
 };
 
--- packages/laconia-event/src/apigateway/ApiGatewayEvent.js
+++ packages/laconia-event/src/apigateway/ApiGatewayEvent.js
@@ -5,7 +5,10 @@ module.exports = class ApiGatewayEvent {
   static fromRaw(event) {
     const apiGatewayEvent = new ApiGatewayEvent();
     apiGatewayEvent.headers = new ApiGatewayInputHeaders(event.headers);
-    apiGatewayEvent.body = parseRequestBody(event, apiGatewayEvent.headers);
+    apiGatewayEvent.body =
+      event.body === null
+        ? null
+        : parseRequestBody(event, apiGatewayEvent.headers);
     apiGatewayEvent.params = Object.assign(
       {},
       event.pathParameters,

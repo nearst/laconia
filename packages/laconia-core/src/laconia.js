dd285d4d3d6523b2fb8b50f025246a345f0fa5fc
Hugo Ribeiro <hugo.ribeiro@net.com.br>
2019-09-10 21:05:12 -0300

    120 - other approach for 120; commit: overloading method register

--- packages/laconia-core/src/laconia.js
+++ packages/laconia-core/src/laconia.js
@@ -18,6 +18,20 @@ const awsInstances = {
   secretsManager: new AWS.SecretsManager()
 };
 
+const ARG_TYPE = {
+  Array: "factories",
+  Function: "fn",
+  Object: "options",
+  String: "name"
+};
+
+const functionToFactory = (name, factory) => {
+  if (name && factory) {
+    factory = laconiaContext => ({ [name]: factory(laconiaContext) });
+  }
+  return factory;
+};
+
 module.exports = app => {
   checkFunction("laconia", app);
   const laconiaContext = new CoreLaconiaContext();
@@ -26,21 +40,37 @@ module.exports = app => {
   const laconia = async (event, context) => {
     laconiaContext.registerInstances({ event, context });
     await laconiaContext.refresh();
-
     return app(event, laconiaContext);
   };
 
   return Object.assign(laconia, {
-    register: (factory, options = {}) => {
-      if (Array.isArray(factory)) {
-        factory.forEach(f => checkFunction("register", f));
-        laconiaContext.registerFactories(factory, options.cache);
-      } else {
-        checkFunction("register", factory);
-        laconiaContext.registerFactory(factory, options.cache);
-      }
+    register: (factoryName, factory, options) => {
+      const args = [factoryName, factory, options].filter(
+        arg => ![null, undefined].includes(arg)
+      );
+      const expectedTypes = ["string", "function", "object"];
+      const {
+        fn,
+        name,
+        factories,
+        options: { cache }
+      } = args
+        .filter(argument => expectedTypes.includes(typeof argument))
+        .map(argument => [ARG_TYPE[argument.constructor.name], argument])
+        .map(([key, argument]) => ({ [key]: argument }))
+        .reduce(Object.assign, {
+          factories: [],
+          options: {}
+        });
+
+      if (!factories.length) factories.push(functionToFactory(name, fn));
+
+      factories.forEach(f => checkFunction("register", { fn: f, args }));
+      laconiaContext.registerFactories(factories, cache);
+
       return laconia;
     },
+
     postProcessor: postProcessor => {
       checkFunction("postProcessor", postProcessor);
       laconiaContext.registerPostProcessor(postProcessor);

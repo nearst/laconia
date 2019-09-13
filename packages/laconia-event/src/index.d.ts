1bd8f952789ed43c63c9929a3b557251bff50c06
Wisen Tanasa <wisen@ceilfors.com>
2019-09-13 06:43:15 +0100

    GH-157 Add more methods under S3 event

--- packages/laconia-event/src/index.d.ts
+++ packages/laconia-event/src/index.d.ts
@@ -1,9 +1,13 @@
 import { S3Event } from "aws-lambda";
+import { Readable } from "stream";
 
 declare namespace event {
   type LaconiaS3Event = {
     bucket: string;
     key: string;
+    getObject: () => any;
+    getJson: () => any;
+    getStream: () => Readable;
   };
   function s3(awsS3Event: S3Event): LaconiaS3Event;
 }
--- packages/laconia-event/test/types/index.ts
+++ packages/laconia-event/test/types/index.ts
@@ -5,4 +5,7 @@ const handler: S3Handler = event => {
   const s3Event = s3(event);
   console.log(s3Event.bucket);
   console.log(s3Event.key);
+  console.log(s3Event.getObject());
+  console.log(s3Event.getJson());
+  s3Event.getStream().pipe(process.stdout);
 };

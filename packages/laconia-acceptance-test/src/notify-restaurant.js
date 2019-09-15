262d60bd1285310ddbd4ac48e42fab958e801b0a
Wisen Tanasa <wisen@ceilfors.com>
2019-09-15 08:52:01 +0100

    GH-120 Use single instance register in acceptance test

--- packages/laconia-acceptance-test/src/notify-restaurant.js
+++ packages/laconia-acceptance-test/src/notify-restaurant.js
@@ -3,13 +3,6 @@ const laconia = require("@laconia/core");
 const { kinesis } = require("@laconia/event");
 const SnsRestaurantNotificationTopic = require("./SnsRestaurantNotificationTopic");
 
-const instances = ({ sns, env }) => ({
-  restaurantNotificationTopic: new SnsRestaurantNotificationTopic(
-    sns,
-    env.RESTAURANT_NOTIFICATION_TOPIC_ARN
-  )
-});
-
 const adapter = app => (event, dependencies) => {
   const orderEvents = kinesis(event).records.map(r => r.jsonData);
   return app(orderEvents, dependencies);
@@ -23,4 +16,11 @@ const app = async (orderEvents, { restaurantNotificationTopic }) => {
   );
 };
 
-exports.handler = laconia(adapter(app)).register(instances);
+exports.handler = laconia(adapter(app)).register(
+  "restaurantNotificationTopic",
+  ({ sns, env }) =>
+    new SnsRestaurantNotificationTopic(
+      sns,
+      env.RESTAURANT_NOTIFICATION_TOPIC_ARN
+    )
+);

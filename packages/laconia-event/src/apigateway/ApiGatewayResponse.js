593aa52288ab3282bc5a00474c5b1510cba75ebc
Hugo Ribeiro <hugo.ribeiro@net.com.br>
2019-10-24 21:29:56 -0300

    using destructuring to improve readability

--- packages/laconia-event/src/apigateway/ApiGatewayResponse.js
+++ packages/laconia-event/src/apigateway/ApiGatewayResponse.js
@@ -2,12 +2,12 @@ const getResponseProps = require("./getResponseProps");
 
 module.exports = class ApiGatewayResponse {
   static create(output, statusCode = 200, headers = {}) {
-    const response = getResponseProps(output);
+    const { getBody, isBase64Encoded, contentType } = getResponseProps(output);
     return Object.assign(new ApiGatewayResponse(), {
       statusCode,
-      body: response.getBody(),
-      isBase64Encoded: response.isBase64Encoded,
-      headers: Object.assign({ "Content-Type": response.contentType }, headers)
+      body: getBody(),
+      isBase64Encoded,
+      headers: Object.assign({ "Content-Type": contentType }, headers)
     });
   }
 };

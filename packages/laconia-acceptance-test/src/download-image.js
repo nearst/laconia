281aa119a61dc6b255b4f6fc1ce236702e2c7ca1
Hugo Ribeiro <hugo.ribeiro@net.com.br>
2019-10-24 21:30:56 -0300

    fixing content type error

--- packages/laconia-acceptance-test/src/download-image.js
+++ packages/laconia-acceptance-test/src/download-image.js
@@ -2,12 +2,13 @@ const fs = require("fs");
 const laconia = require("@laconia/core");
 const adapterApi = require("@laconia/adapter-api");
 
-const apigateway = adapterApi.apigateway({});
+const apigateway = adapterApi.apigateway({
+  responseAdditionalHeaders: {
+    "Content-Type": "image/png"
+  }
+});
 const imgPath = `${__dirname}/resources/2_vertical@0.5x.png`;
 
-exports.app = async () => ({
-  body: fs.readFileSync(imgPath),
-  statusCode: 200
-});
+exports.app = () => fs.readFileSync(imgPath);
 
 exports.handler = laconia(apigateway(exports.app));

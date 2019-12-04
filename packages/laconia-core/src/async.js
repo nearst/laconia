603d81046ae1591229cfb74583c1e10e2dcf7fd7
Hugo Ribeiro <hugo.ribeiro@net.com.br>
2019-12-04 17:28:35 -0300

    laconia#48 - serverless-offline raises warning due to callback usage; commit: remove async wrapper

--- packages/laconia-core/src/async.js
+++
@@ -1,7 +0,0 @@
-const laconia = require("./laconia");
-
-module.exports = app => {
-  const laconiaSync = laconia(app);
-  const laconiaAsync = async (event, context) => laconiaSync(event, context);
-  return Object.assign(laconiaAsync, laconiaSync);
-};

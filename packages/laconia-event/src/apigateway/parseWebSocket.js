efd0f62e36b7ab563c69a69dc928381e17a434f9
Hugo Sena Ribeiro <hugosenari@gmail.com>
2020-02-17 06:28:28 -0300

    fix(laconia-event): only try to read body as Buffer if it isn't falsy (#541)
    WebSocket: Error 500 when using adapter for $connect route #49
     The problem is that we are trying
    to buffer event.body (undefined).
    
    fix #49

--- packages/laconia-event/src/apigateway/parseWebSocket.js
+++ packages/laconia-event/src/apigateway/parseWebSocket.js
@@ -2,6 +2,6 @@ const getBody = require("./getBody");
 const tryParseJson = require("../tryParseJson");
 
 module.exports = event => {
-  const body = getBody(event);
+  const body = event.body && getBody(event);
   return tryParseJson(body);
 };

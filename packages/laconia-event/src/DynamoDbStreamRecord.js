6d5d143e641302ef12581ec9ad48958090ac9fc0
Ankcorn <thomasankcorn@gmail.com>
2020-07-10 23:46:23 +0100

    feat: parseRecord getter on DynamoDbStreamRecord

--- packages/laconia-event/src/DynamoDbStreamRecord.js
+++ packages/laconia-event/src/DynamoDbStreamRecord.js
@@ -14,6 +14,16 @@ module.exports = class DynamoDbStreamRecord {
     return this.data.NewImage;
   }
 
+  get parseRecord() {
+    return {
+      eventName: this.eventName,
+      StreamViewType: this.data.StreamViewType,
+      Keys: this.data && this.data.Keys ? converter.unmarshall(this.data.Keys) : {},
+      NewImage: this.data && this.data.NewImage ? converter.unmarshall(this.data.NewImage) : {},
+      OldImage: this.data && this.data.OldImage ? converter.unmarshall(this.data.OldImage) : {}
+    }
+  }
+
   static fromRaw(record) {
     return new DynamoDbStreamRecord(record.dynamodb);
   }

af6072f73b325817080b02c92200bed919c9b341
Thomas Schoffelen <thomas@schof.co>
2025-03-21 14:22:51 +0000

    feat(core): remove aws-sdk default dependencies

--- packages/laconia-core/src/laconia.js
+++ packages/laconia-core/src/laconia.js
@@ -1,19 +1,9 @@
-const AWS = require("aws-sdk");
 const CoreLaconiaContext = require("./CoreLaconiaContext");
 const { checkFunction, checkFunctionOrObject } = require("./typeChecking");
-
-const awsInstances = {
-  lambda: new AWS.Lambda(),
-  s3: new AWS.S3(),
-  ssm: new AWS.SSM(),
-  sns: new AWS.SNS(),
-  secretsManager: new AWS.SecretsManager()
-};
 
 module.exports = app => {
   checkFunction("laconia", app);
   const laconiaContext = new CoreLaconiaContext();
-  laconiaContext.registerBuiltInInstances(awsInstances);
 
   const laconia = async (event, context, callback) => {
     laconiaContext.registerInstances({ event, context });
